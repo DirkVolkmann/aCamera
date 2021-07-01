56f54bab5fcd71d1d31c1565086cc5a23d8f7bf2
Dirk <64075699+DirkVolkmann@users.noreply.github.com>
2021-07-01 23:10:17 +0200

    Added Settings Fragment
    not final

--- app/src/main/java/com/dirk/acamera/fragments/SettingsFragment.kt
+++ app/src/main/java/com/dirk/acamera/fragments/SettingsFragment.kt
@@ -1,10 +1,45 @@
 package com.dirk.acamera.fragments
 
 import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.preference.PreferenceFragmentCompat
 import com.dirk.acamera.R
 
+private const val TAG = "aCamera SettingsFragment"
+
 class SettingsFragment : PreferenceFragmentCompat() {
+
+    // Views
+    private lateinit var container: ConstraintLayout
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Log.d(TAG, "onCreate called")
+        /*if (savedInstanceState == null) {
+            requireActivity().supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.settings_container, SettingsFragment())
+                .commit()
+        }*/
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? = inflater.inflate(R.layout.fragment_settings, container, false)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        Log.d(TAG, "onViewCreated called")
+
+        // Initialize views
+        container = view as ConstraintLayout
+    }
 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.root_preferences, rootKey)
